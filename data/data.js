124c6d9b63da1101284c19175bb18da2a610c497
GabrielDev0101 <128307365+GabrielDev0101@users.noreply.github.com>
2023-11-27 08:23:20 -0300

    Create data.js

---
+++ data/data.js
@@ -0,0 +1,60 @@
+export const data = [
+
+    {
+        question: 'which of the following options is not a text editor?',
+        choices: [
+            'vim',
+            'vscode',
+            'emacs',
+            'word',
+            'jetbrains'
+        ],
+        answer: 'word'
+    },
+
+    {
+        question: 'Which language is not object oriented?',
+        choices: [
+            'java',
+            'haskell',
+            'c++',
+            'python'
+        ],
+        answer: 'haskell'
+    },
+
+    {
+        question: 'what language are not used to use on the web?',
+        choices: [
+            'stylus',
+            'less',
+            'sass',
+            'PostCSS',
+            'nextCSS'
+        ],
+        answer: 'nextCSS'
+    },
+
+    {
+        question: 'Which of the following is a server-side programming language?',
+        choices: [
+            'JavaScript',
+            'Python',
+            'Ruby',
+            'HTML'
+        ],
+        answer: 'HTML'
+    },
+
+    {
+        question: 'What is the purpose of the CSS property "z-index"?',
+        choices: [
+            'To control the font size',
+            'To control the vertical spacing between elements',
+            'To control the order of overlapping elements',
+            'To control the background color'
+        ],
+        answer: 'To control the order of overlapping elements'
+    }
+
+]
